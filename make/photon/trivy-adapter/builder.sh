a4d7ac5fa667562d7276fbc29e3683a7fa4094bb
Mitsuru Kariya <mitsuru.kariya@nttdata.com>
2024-04-23 10:33:35 +0900

    WIP

--- make/photon/trivy-adapter/builder.sh
+++ make/photon/trivy-adapter/builder.sh
@@ -18,17 +18,19 @@ cur=$PWD
 TEMP=$(mktemp -d ${TMPDIR-/tmp}/trivy-adapter.XXXXXX)
 git clone https://github.com/aquasecurity/harbor-scanner-trivy.git $TEMP
 cd $TEMP; git checkout $VERSION; cd -
-
-echo "Building Trivy adapter binary based on golang:1.21.8..."
 cp Dockerfile.binary $TEMP
-docker build -f $TEMP/Dockerfile.binary -t trivy-adapter-golang $TEMP
 
-echo "Copying Trivy adapter binary from the container to the local directory..."
-ID=$(docker create trivy-adapter-golang)
-docker cp $ID:/go/src/github.com/aquasecurity/harbor-scanner-trivy/scanner-trivy binary
+for arch in amd64 arm64; do
+	echo "Building Trivy adapter binary based on golang:1.21.8..."
+	docker build -f $TEMP/Dockerfile.binary -t trivy-adapter-golang --build-arg GOARCH=$arch $TEMP
 
-docker rm -f $ID
-docker rmi -f trivy-adapter-golang
+	echo "Copying Trivy adapter binary from the container to the local directory..."
+	ID=$(docker create trivy-adapter-golang)
+	docker cp $ID:/go/src/github.com/aquasecurity/harbor-scanner-trivy/scanner-trivy binary/$arch
+
+	docker rm -f $ID
+	docker rmi -f trivy-adapter-golang
+done
 
 echo "Building Trivy adapter binary finished successfully"
 cd $cur
